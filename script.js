1ec0d56d9cda834a776e6fc5568187caa678d1bb
Daniel <danielson@college.harvard.edu>
2023-07-10 14:25:47 -0400

    finishing game

--- script.js
+++ script.js
@@ -1,14 +1,271 @@
-
-
-
-
 var titles = ['Back To December', 'Better Than Revenge', 'Castles Crumbling', 'Dear John', 'Electric Touch', 'Enchanted', 'Foolish One', 
               'Haunted', 'I Can See You', 'Innocent', 'Last Kiss', 'Long Live', 'Mean', 'Mine', 'Never Grow Up', 'Ours', 'Sparks Fly', 'Speak Now', 
               'Superman', 'The Story Of Us', 'Timeless', 'When Emma Falls in Love'];
 
 
-var audios = ['tracks/Back To December.mp4', 'tracks/Better Than Revenge.mp4', 'tracks/Castles Crumbling.mp4', 'tracks/Dear John.mp4', 
-                'tracks/Electric Touch.mp4', 'tracks/Enchanted.mp4', 'tracks/Foolish One.mp4', 'tracks/Haunted.mp4', 'tracks/I Can See You.mp4', 
-                'tracks/Innocent.mp4', 'tracks/Last Kiss.mp4', 'tracks/Long Live.mp4', 'tracks/Mean.mp4', 'tracks/Mine.mp4', 'tracks/Never Grow Up.mp4',
-                'tracks/Ours.mp4', 'tracks/Sparks Fly.mp4', 'tracks/Speak Now.mp4', 'tracks/Superman.mp4', 'tracks/The Story Of Us.mp4', 'tracks/Timeless.mp4',
-                'tracks/When Emma Falls in Love.mp4'];
+var audios = ['tracks/Back To December.mp3', 'tracks/Better Than Revenge.mp3', 'tracks/Castles Crumbling.mp3', 'tracks/Dear John.mp3', 
+                'tracks/Electric Touch.mp3', 'tracks/Enchanted.mp3', 'tracks/Foolish One.mp3', 'tracks/Haunted.mp3', 'tracks/I Can See You.mp3', 
+                'tracks/Innocent.mp3', 'tracks/Last Kiss.mp3', 'tracks/Long Live.mp3', 'tracks/Mean.mp3', 'tracks/Mine.mp3', 'tracks/Never Grow Up.mp3',
+                'tracks/Ours.mp3', 'tracks/Sparks Fly.mp3', 'tracks/Speak Now.mp3', 'tracks/Superman.mp3', 'tracks/The Story Of Us.mp3', 'tracks/Timeless.mp3',
+                'tracks/When Emma Falls in Love.mp3'];
+
+var questions = [
+    {
+        question: 'btd',
+        correctAnswer: 'back to december'
+    },
+    {
+        question: 'btr',
+        correctAnswer: 'better than revenge'
+    },
+    {
+        question: 'cc',
+        correctAnswer: 'castles crumbling'
+    },
+    {
+        question: 'dj',
+        correctAnswer: 'dear john'
+    },
+    {
+        question: 'et',
+        correctAnswer: 'electric touch'
+    },
+    {
+        question: 'e',
+        correctAnswer: 'enchanted'
+    },
+    {
+        question: 'fo',
+        correctAnswer: 'foolish one'
+    },
+    {
+        question: 'h',
+        correctAnswer: 'haunted'
+    },
+    {
+        question: 'icsy', 
+        correctAnswer: 'i can see you'
+    },
+    {
+        question: 'i',
+        correctAnswer: 'innocent'
+    },
+    {
+        question: 'lk',
+        correctAnswer: 'last kiss'
+    },
+    {
+        question: 'll',
+        correctAnswer: 'long live'
+    },
+    {
+        question: 'mean', 
+        correctAnswer: 'mean'
+    },
+    {
+        question: 'mine',
+        correctAnswer: 'mine'
+    },
+    {
+        question: 'ngu',
+        correctAnswer: 'never grow up'
+    },
+    {
+        question: 'o',
+        correctAnswer: 'ours'
+    },
+    {
+        question: 'sf', 
+        correctAnswer: 'sparks fly'
+    },
+    {
+        question: 'sn',
+        correctAnswer: 'speak now'
+    },
+    {
+        question: 's',
+        correctAnswer: 'superman'
+    },
+    {
+        question: 'tsou',
+        correctAnswer: 'the story of us'
+    },
+    {
+        question: 't',
+        correctAnswer: 'timeless'
+    },
+    {
+        question: 'wefil',
+        correctAnswer: 'when emma falls in love'
+    }
+];
+
+// var questions = [
+//     {
+//         question: 'Back To December',
+//         correctAnswer: 'back to december',
+//         audio: new Audio('tracks/Back To December.mp3')
+//     },
+//     {
+//         question: 'Better Than Revenge',
+//         correctAnswer: 'better than revenge',
+//         audio: new Audio('tracks/Better Than Revenge.mp3')
+//     },
+//     // ... add other questions with audio elements
+// ];
+
+
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const photo = document.getElementsByClassName("TS");
+const scoreValueElement = document.getElementById("scoreValue");
+const submitButton = document.getElementById("submit-btn");
+const answerField = document.getElementById("answerField");
+const songContainerElement = document.getElementById("song-container");
+const choicesContainer = document.getElementById("choices");
+const line = document.getElementById("line");
+
+let selectedChoices = [];
+
+var sad = [
+    "sad/sad0",
+    "sad/sad1",
+    "sad/sad2",
+    "sad/sad3",
+    "sad/sad4",
+];
+
+var happy = [
+    "happy/happy0",
+    "happy/happy1",
+    "happy/happy2",
+    "happy/happy3",
+    "happy/happy4",
+    "happy/happy5",
+    "happy/happy6",
+];
+
+var score = 0;
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+submitButton.addEventListener("click", submitAnswer);
+
+function getRandomNumber(minimum, maximum) {
+    minimum = Math.ceil(minimum);
+    maximum = Math.floor(maximum);
+
+    return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
+
+function startGame() {
+    startButton.classList.add("hide");
+
+    // selectedChoices = Array.from(choicesContainer.querySelectorAll('input[type="checkbox"]:checked')).map(input => input.value);
+
+    // let newQuestions = [];
+    // selectedChoices.forEach(element => {
+    //     newQuestions = newQuestions.concat([element]);
+    // });
+
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide");
+    setNextQuestion();
+}
+
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+
+    answerField.value = "";
+    answerField.classList.remove("correct-answer", "wrong-answer");
+    submitButton.disabled = false;
+}
+
+function showQuestion(question) {
+
+    var audio = document.getElementById(question.question);
+    // audio.play();
+    setTimeout(function(){
+        audio.play();
+    
+        setTimeout(function(){
+            audio.pause();
+            audio.currentTime = 0;
+        }, 2000);
+    }, 1000);
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    document.getElementById(
+        "img"
+    ).src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSemGOaBH1ipIjvagC2qZb5DCfvlSAISV0p2A&usqp=CAU";
+    nextButton.classList.add("hide");
+}
+
+function submitAnswer(event) {
+    event.preventDefault();
+
+    let input = answerField.value.toLowerCase();
+    const correctAnswer = shuffledQuestions[currentQuestionIndex].correctAnswer;
+    setStatusClass(document.body, input === correctAnswer);
+
+    if (input === correctAnswer) {
+        changeImage(happy[getRandomNumber(0, 6)]);
+        answerField.classList.add("correct-answer");
+        score++;
+        scoreValueElement.innerText = score.toString();
+    } else {
+        alert("stoopid be better");
+        answerField.classList.add("wrong-answer");
+        answerField.value = correctAnswer;
+        changeImage(sad[getRandomNumber(0, 4)]);
+    }
+
+    submitButton.disabled = true;
+
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide");
+    } else {
+        const end = score;
+        questionElement.innerText = `You got ${end}/${shuffledQuestions.length}`;
+        submitButton.classList.add("hide")
+        songContainerElement.classList.remove("hide");
+        line.classList.remove("hide");
+        startButton.innerText = "Restart";
+        startButton.classList.remove("hide");
+        startButton.addEventListener("click", () => {
+        score = 0;
+        scoreValueElement.innerText = score
+        });
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+}
+
+function changeImage(a) {
+    document.getElementById("img").src = a + ".jpeg";
+}
+  
